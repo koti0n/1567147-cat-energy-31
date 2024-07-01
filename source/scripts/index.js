d67bc268774858de27086cd4199c36e56db73256
Kozonova Albina <166506551+koti0n@users.noreply.github.com>
2024-07-01 15:37:21 +1000

    Module5-task1 (#6)
    * добавляет векторные изображения и частично сетки
    
    * сетки
    
    * доработка сеток
    
    * доработка сайта

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,8 @@
 /* в этот файл добавляет скрипты*/
+
+const button = document.querySelector('.main-nav__toggle');
+const navigation = document.querySelector('.main-nav');
+
+button.onclick = function(){
+  navigation.classList.toggle('main-nav--closed');
+};
